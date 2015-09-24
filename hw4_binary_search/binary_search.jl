523dd455ac6cb70226c36e390937e4fa4056c049
[jiningq] <[jiningq@andrew.cmu.edu]>
2015-09-24 01:25:01 -0400

    Homework for 0924
    Added partition_point function. Added argument for partition_point
    function in the binary_search function

--- hw4_binary_search/binary_search.jl
+++ hw4_binary_search/binary_search.jl
@@ -5,29 +5,25 @@
 # Binary search
 # (1)
 using Base.Test
-using DataFrames
+
+@test_throws ErrorException binary_search(1, [2,3,4], 4, 1)
+@test_throws ErrorException binary_search(1, [3,2,4], 1, 4)
+@test binary_search(1, [-1,0,1], 1, 4) == (true, 3)
+@test binary_search(1, [2,3,4], 1, 4) == (false, 0)
+@test binary_search(1, [-1,0,1], 1, 3) == (false, 0)
+@test binary_search(4, [1,2,3,5,6], 1, 6) == (false, 0)
+@test binary_search(2, [1,2], 1, 3) == (true, 2)
+@test binary_search(2, [1,2], 1, 2) == (false, 0)
+@test binary_search(1, linspace(1, 100, 100), 1, 2) == (true, 1)
+@test binary_search(100, linspace(1, 100, 100), 1, 100) == (false, 0)
+@test binary_search(99, linspace(1, 100, 100),1 , 100) == (true, 99)
 
 
-@test_throws ErrorException binary_search_w(1, [2,3,4], 4, 1)
-@test_throws ErrorException binary_search_w(1, [3,2,4], 1, 4)
-@test binary_search_w(1, [-1,0,1], 1, 4) == (true, 3)
-@test binary_search_w(1, [2,3,4], 1, 4) == (false, 0)
-@test binary_search_w(1, [-1,0,1], 1, 3) == (false, 0)
-@test binary_search_w(4, [1,2,3,5,6], 1, 6) == (false, 0)
-@test binary_search_w(2, [1,2], 1, 3) == (true, 2)
-@test binary_search_w(2, [1,2], 1, 2) == (false, 0)
-@test binary_search_w(4, linspace(1, 100, 100), 1, 100) == (true, 4)
-@test binary_search_w(100, linspace(1, 100, 100), 1, 100) == (false, 0)
-@test binary_search_w(99, linspace(1, 100, 100),1 , 100) == (true, 99)
 
 
-function midpoint(a, b)
-  return floor((a + b) / 2)
-end
 
-function binary_search_w(query, seq, first, last)
-  found = false
-  bound = 0
+
+function binary_search(query, seq, first, last, partition_point = partition_point)
   if first > last - 1
     error("Invalid input: empty range between bounds.")
   end
@@ -36,11 +32,11 @@ function binary_search_w(query, seq, first, last)
   end
 
   if query < seq[first] || query > seq[last - 1]
-    return (found,bound)
+    return (false,0)
   end
 
   while first < last - 1
-    imid = midpoint(first , last - 1)
+    imid = partition_point(first , last - 1)
     if seq[imid] < query
       first = imid + 1
     else last = imid + 1
@@ -49,8 +45,40 @@ function binary_search_w(query, seq, first, last)
   if first == last - 1 && seq[first] == query
     found = true
     bound = first
-    return (found,bound)
+    return (true,bound)
   else
-    return (found,bound)
+    return (false,0)
   end
 end
+
+
+#(2)
+# writing the partition point function
+# Assuming we have general iterators instead of integers, I use linear scan for finding the partition point
+
+function partition_point(pred, seq, first, last)
+  it = seq[ first]
+  while pred( seq [it]) == true
+    it=advance( it)
+  end
+  return it
+end
+
+
+# Assuming integer indices then the partition point would be much simpler
+# Use multiple dispatch for this particular case
+# In fact, the integer case would be equivalent to setting
+# pred = ( x -> x < floor ( (first + last )/2 ) )
+# advance = ( it -> it + 1)
+
+
+@test partition_point(1, 3) == 2
+@test partition_point(4, 81) ==  42
+
+partition_point(a::Number,b::Number) = floor( (a + b)/2 )
+
+# Honestly I am a bit confused about this question. Is what I did what you wanted?
+# I am not exactly sure whether I should sacrifice performance for generality here
+# in the integer index case. Also, I'm not sure how I can test the general iterator
+# version partition_point function I wrote above.
+
