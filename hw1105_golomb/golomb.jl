dc0b617075b210b9b751affccbfde725ba8faafd
[jiningq] <[jiningq@andrew.cmu.edu]>
2015-11-06 12:22:33 -0500

    Revised algorithm

--- hw1105_golomb/golomb.jl
+++ hw1105_golomb/golomb.jl
@@ -1,6 +1,7 @@
 #using Memoize
 using StatsBase
 using Base.Test
+using PyPlot
 
 # Naive implementation based on recursion.
 # Takes 2 seconds to evaluate 60. Takes 21 seconds to evaluate 70.
@@ -12,7 +13,7 @@ end
 # Using the memoize package to get a memoized version of the function
 # Weird 'stack overflow' problem for large n's, 1e6 for example
 # The odd thing is, if we grow the value of n gradually, it wouldn't
-#give the same stack overflow error anymore.
+# give the same stack overflow error anymore.
 
 # @memoize function golomb1(n)
 #   n == 1 ? 1 : 1 + golomb1(n - golomb1(golomb1(n-1)))
@@ -59,27 +60,106 @@ end
 
 # This is written based on this answer on stackoverflow,
 # http://stackoverflow.com/questions/12786087/golombs-sequence
-# but it seems at large n value it still has O(n) complexity
+# It is very fast with lower n value. The time complexity seems
+# to be O(log(n)) at smaller n (<1e5, e.g.) with several spikes,
+# the spikes become much more frequent and the time is starting
+# to look like growing linearly.
+
+# I attached a plot produced.
+
 function golomb3(n)
-  if n < 1000
-    return golomb2(n)
-  else
-    sumTemp = k = 0
-    while sumTemp < n
-      k += 1
-      sumTemp += k * golomb2(k)
+  if n <= 3
+    return n == 1 ? 1: 2
+  end
+  i = 1
+  G = [0 => 1, 1 => 2, 2=> 4]
+    while G[G[i]-1] < n + 1
+    for j in G[i]:G[i+1] - 1
+      G[j] = G[j - 1] + i + 1
+      if G[j] >= n + 1
+        return j
+      end
     end
-    sumTemp -= k * golomb2(k)
-    l = golomb2(sumTemp)
-    while sumTemp <= n
-      l += 1
-      sumTemp += k
-    end
-    return l
+    i = i + 1
+  end
+end
+
+# function for measuring the time
+function time_f(f, x, leng = x, omit = false, Log = false)
+    Range = int(linspace(1, x, int(leng)))
+  time_naive = zeros(length(Range))
+  for jj in 1:length(Range)
+    tic()
+    f(Range[jj])
+    time_naive[jj] = toq()
+#     tic()
+#     golomb2(jj)
+#     time_mem[jj] = toq()
+  end
+  println(sum(time_naive .> 0.02))
+  if(omit)
+    Range = Range[time_naive .< 0.02]
+    time_naive = time_naive[time_naive .< 0.02]
+
+  end
+  Log ? PyPlot.plot(log(Range)/log(10),time_naive):PyPlot.plot(Range,time_naive)
+  Log ? PyPlot.xlabel("lg(n)"): PyPlot.xlabel("n")
+  PyPlot.ylabel("Time (seconds)")
+  PyPlot.legend()
+  show()
+end
+
+@time time_f(golomb3, 5e5, 1e4, true, true)
+@time time_f(golomb3,5e7, 2e3, false, false)
+
+i = 2
+10^i
+function t(n)
+  tic()
+  golomb3(n)
+  return(toq())
+end
+
+
+# Attempt to use the asymptotic formula for the sequence, no good so far.
+
+i = 1000
+1e6 == 10^6
+while t(i^3) <
+  println(i)
+  i = i + 1
+end
+phi = (sqrt(5)+1)/2
+function f(n)
+  (phi^(2-phi) * n^(phi - 1)) # +n^(phi-1)/log(n)
+end
+@time golomb3(1e8)
+
+function err(n)
+  n^(phi-1)/log(n)
+end
+
+
+@time errar = [f(n)-golomb3(n) for n in 1e8:1e8+1e3]
+PyPlot.plot(1e8:1e8+1e3, errar)
+show()
+
+f(100)-golomb3(100)
+
+i = 1
+Min = Max = 0
+while max(-Min, Max) < 50
+  i = i + 1
+  if f(i) - golomb3(i) < Min
+    Min = f(i) - golomb3(i)
+    println(Max)
+  elseif f(i) - golomb3(i) > Max
+    Max = f(i) - golomb3(i)
+    println((i, Max))
   end
 end
 
 
-# the speed achieved by this algorithm is still only remote from solving
-# problem 341 of Project Euler. Would you let me know what a faster ones
-# would be like? Say a log time algorithm?
+
+
+# http://www.sciencedirect.com/science/article/pii/0022314X9290024J
