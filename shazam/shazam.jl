2ee5472e0affa3a0a58c9721ffb65bbf8f4a8fe9
[jiningq] <[jiningq@andrew.cmu.edu]>
2015-11-16 17:57:07 -0500

    Signature, lib building, matching
    Didn’t do database for now. Using a hash table as music library.

--- shazam/shazam.jl
+++ shazam/shazam.jl
@@ -1,14 +1,16 @@
 cd("dropbox/computing")
 
-using LSH
-using DSP, WAV, PyPlot, LSH
 
-s, fs = wavread("track1.wav");
+using DSP, WAV, PyPlot
+
+s, fs = wavread("music/Summer.wav");
 
 # function which given a segment of wav file, returns its power spectrum
-function gen_spec(s, window = hanning, h = 10)
-   power(periodogram(s[1:round(Int, fs * h)] .* window(h * round(Int, fs))))
+function gen_spec(s, Fs::Integer, window = hanning, h = 10)
+   power(periodogram(s[1:round(Int, Fs * h)], window = window))
 end
+
+# gen_spec(s[1:10*fs], fs)
 
 # the function that takes in a power spectrum, takes a range of fequency 
 # (up to 5e4 here) , separates it into n(10 by default) non-overlapping 
@@ -20,12 +22,45 @@ end
 
 
 # function that puts it together: takes in file name, compute the signature
-# of the file by hashing the peaks of power spectrum in each (overlapping)
+# of the file by hashing the peaks of power spectrum in each (non-overlapping)
 # window.
 
-function gen_signature(file:: ASCIIString, fs:: Integer, hash = lsh, window = hanning, delta = 1, h = 10)
-  s = wavread(file)
-  [lsh(pick_peaks(gen_spec(s[round(Int, 1 + i * fs * delta) : round(Int, (i + 1) * fs * delta)]))) 
+function gen_signature(file:: ASCIIString, hash = hash, window = hanning, delta = 1, h = 10)
+  s, fs= wavread(file)
+  fs = round(Int, fs)
+  [hash(pick_peaks(gen_spec(s[round(Int, 1 + i * fs * delta) : round(Int, i * fs * delta + fs * h)], fs))) 
   for i in 0 : floor(size(s)[1] / (fs * delta) - h)]
 end
 
+# first try to store the music entries as hash tables in the RAM
+function build_lib(folder:: ASCIIString)
+  Music_lib = Dict()
+  file_list = readdir(folder)
+  for name in file_list[2 : end]
+    println(string(folder,"/",name))
+    for j in gen_signature(string(folder,"/",name))
+      Music_lib[j] = name
+    end
+  end
+  return Music_lib
+end
+
+
+
+function match_snippet(file:: ASCIIString, lib:: Dict)
+  a = gen_signature(file)
+  if all(map(x -> !haskey(lib, x), a))
+    return("Song not in the library!")
+  else
+    #return(a[map(x -> haskey(lib, x), a)])
+    return(lib[a[map(x -> haskey(lib, x), a)][1]])
+  end
+end
+
+
+# I'll write the version with database instead of hashtable later.
+
+# using it
+
+music_lib = build_lib("music")
+match_snippet("test.wav", music_lib)
